98474238537d2dfe44ae3704694e2f07f19a9a53
Simon Grabbe <131392527+GameBoyPlay517@users.noreply.github.com>
2026-05-26 13:23:56 -0400

    Converted from blueprints to C++
    YIPPIE YAY

---
+++ Source/ChemProjRepo/NotImportant.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CHEMPROJREPO_API NotImportant
+{
+public:
+	NotImportant();
+	~NotImportant();
+};
