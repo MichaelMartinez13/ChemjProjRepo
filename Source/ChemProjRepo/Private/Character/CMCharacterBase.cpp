d4fcf8778d27ead6c14a9ebb00bb7ddfa492622c
Simon Grabbe <131392527+GameBoyPlay517@users.noreply.github.com>
2026-06-01 15:24:53 -0400

    FirstPersonCamera and Movement
    - Added new Debug level
    - Added Player Movement

---
+++ Source/ChemProjRepo/Private/Character/CMCharacterBase.cpp
@@ -0,0 +1,114 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CMCharacterBase.h"
+
+// Sets default values
+ACMCharacterBase::ACMCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	PlayerArrow->SetupAttachment(GetRootComponent());
+	PlayerArrow->SetRelativeRotation(FRotator::ZeroRotator);
+
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+
+	if (!IsValid(FirstPersonCameraComponent)) return;
+	
+	PlayerMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("PlayerMesh"));
+
+	if (!IsValid(PlayerMesh)) return;
+
+	
+	PlayerMesh->SetupAttachment(GetMesh());
+
+	PlayerMesh->FirstPersonPrimitiveType = EFirstPersonPrimitiveType::FirstPerson;
+	PlayerMesh->SetCollisionProfileName(FName("NoCollision"));
+	GetMesh()->FirstPersonPrimitiveType = EFirstPersonPrimitiveType::WorldSpaceRepresentation;
+
+	PlayerMesh->SetOnlyOwnerSee(true);
+	GetMesh()->CastShadow = true;
+	GetMesh()->bCastHiddenShadow = true;
+	FirstPersonCameraComponent->SetupAttachment(PlayerMesh, FName("head"));
+
+	FirstPersonCameraComponent->SetRelativeLocationAndRotation(FirstPersonCameraOffset, FRotator(0.0, 90.f, -90.f));
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	FirstPersonCameraComponent->bEnableFirstPersonFieldOfView = true;
+	FirstPersonCameraComponent->bEnableFirstPersonScale = true;
+	FirstPersonCameraComponent->FirstPersonFieldOfView = FirstPersonFOV;
+	FirstPersonCameraComponent->FirstPersonScale = FirstPersonViewScale;
+
+}
+
+// Called when the game starts or when spawned
+void ACMCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	check(GEngine != nullptr);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(FirstPersonContext, 0);
+		}
+	}
+	
+}
+
+
+// Called every frame
+void ACMCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACMCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) 
+	{
+		//Movement
+		EnhancedInputComponent->BindAction(PlayerMove, ETriggerEvent::Triggered, this, &ACMCharacterBase::Move);
+	
+		//Looking
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ACMCharacterBase::Look);
+	}
+
+}
+
+void ACMCharacterBase::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+
+	
+	if (!Controller) return; 
+
+	const FVector ForwardVector = PlayerArrow->GetForwardVector();
+	AddMovementInput(ForwardVector, MovementValue.Y);
+
+	const FVector RightVector = PlayerArrow->GetRightVector();
+	AddMovementInput(RightVector, MovementValue.X);
+
+	
+
+}
+
+void ACMCharacterBase::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>();
+
+	if (!Controller) return;
+
+	AddControllerPitchInput(LookAxisValue.Y);
+	AddControllerYawInput(LookAxisValue.X);
+}
+
---
+++ Source/ChemProjRepo/Public/Character/CMCharacterBase.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Components/ArrowComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
+#include "InputTriggers.h"
+#include "Camera/CameraComponent.h"
+#include "CMCharacterBase.generated.h"
+
+
+
+class UInputMappingContext;
+class UInputAction; 
+class UInputComponent;
+
+UCLASS()
+class CHEMPROJREPO_API ACMCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACMCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> FirstPersonContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> PlayerMove;
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Chained|Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UArrowComponent> PlayerArrow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input | Camera")
+	TObjectPtr<USkeletalMeshComponent> PlayerMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input | Camera")
+	TObjectPtr<UCameraComponent> FirstPersonCameraComponent;
+
+	UPROPERTY(EditAnywhere, Category = "Input | Camera")
+	FVector FirstPersonCameraOffset = FVector(2.8f, 5.9f, 0.0f);
+
+	UPROPERTY(EditAnywhere, Category = "Input | Camera")
+	float FirstPersonFOV = 70.f;
+
+	UPROPERTY(EditAnywhere, Category = "Input | Camera")
+	float FirstPersonViewScale = 0.6f;
+
+};
