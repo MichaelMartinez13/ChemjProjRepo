d8beffc9a468750bfb54fbdac9e6d6a2137a6d67
snn173 <samuelcodes@outlook.com>
2026-06-23 14:52:13 -0400

    converted base shippable product to c++

---
+++ Source/ChemProjRepo/Private/ConveyorSystem/CMBaseShippableProduct.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ConveyorSystem/CMBaseShippableProduct.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+ACMBaseShippableProduct::ACMBaseShippableProduct() {
+	BoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("BaseCollision"));
+	SetRootComponent(BoxCollision);
+
+	BoxCollision->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f));
+	BoxCollision->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	BoxCollision->SetCollisionObjectType(ECollisionChannel::ECC_PhysicsBody);
+	BoxCollision->SetCollisionResponseToAllChannels(ECR_Block);
+	BoxCollision->SetGenerateOverlapEvents(true);
+	BoxCollision->SetSimulatePhysics(true);
+
+	BoxCollision->BodyInstance.bLockXRotation = true;
+	BoxCollision->BodyInstance.bLockYRotation = true;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseStaticMesh"));
+	StaticMesh->SetupAttachment(BoxCollision);
+	StaticMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
