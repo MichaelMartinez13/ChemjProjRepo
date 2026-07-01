7db9c5f11d0248d54ed119d15f63c9c573b2d1da
Simon Grabbe <131392527+GameBoyPlay517@users.noreply.github.com>
2026-07-01 14:45:49 -0400

    added a crafting cube

---
+++ Source/ChemProjRepo/Private/CraftingSystem/CMChemicalCraftingTable.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CraftingSystem/CMChemicalCraftingTable.h"
+
+// Sets default values
+ACMChemicalCraftingTable::ACMChemicalCraftingTable()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	ChemicalCraftingTable = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CraftingTable"));
+	ChemicalCraftingTable->SetupAttachment(GetRootComponent());
+	ChemicalCraftingTable->SetRelativeRotation(FRotator::ZeroRotator);
+
+}
+
+// Called when the game starts or when spawned
+void ACMChemicalCraftingTable::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACMChemicalCraftingTable::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
